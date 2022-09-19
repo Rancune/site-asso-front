9857f48482f634279686eb19b09969aadf3d06b7
Rancune <3519851+Rancune@users.noreply.github.com>
2022-09-19 15:36:44 +0200

    .js files conversion to .tsx files

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img
+          src="http://img.over-blog.com/500x250/2/28/81/89/Baniere-blog2-copie.png"
+          className="logo"
+          alt="logo"
+        />
+        <p>alpsjaa</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
