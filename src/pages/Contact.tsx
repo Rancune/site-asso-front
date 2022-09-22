6fbcdd0645f65b6324545e52f96e4e1ff0b7b7e5
Rancune <3519851+Rancune@users.noreply.github.com>
2022-09-22 21:33:01 +0200

    setup pages Contact and Services

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,13 +1,54 @@
+import { Box, Stack, Typography } from "@mui/material";
 import React from "react";
+import Footer from "../components/footer/Footer";
 import Navbar from "../components/header/Navbar";
+import Image from "../images/adress.png";
 
 const Contact = () => {
   return (
     <>
       <Navbar />
-      <div>Contact</div>
+      <Typography variant="h1" align="center">
+        Contact
+      </Typography>
+      <Stack
+        direction={"row"}
+        margin="auto"
+        width={"80%"}
+        display="flex"
+        padding={6}
+        height={"auto"}>
+        <Stack spacing={6}>
+          <Typography variant="h4" align="left" justifyContent={"center"}>
+            1) Accès piéton 21 avenue de Pierroton (chemin de terre entre le 12
+            et le 14)
+          </Typography>
+          <Typography variant="h4" align="left">
+            2) Pour ceux qui viennent en voiture il est préférable de se garer
+            côté voie ferrée, accès par le stade Chambrelant.
+          </Typography>
+          <Typography variant="h4" align="left">
+            3) Depuis la A63 Sortie 24 direction Pierroton; au rond-point tout
+            droit jusqu'au pont de chemin de fer; immédiatement après le pont,
+            tourner à gauche; passer devant le stade Chambrelant, aller jusqu'à
+            la voie ferrée et tourner à droite; continuer sur le chemin le
+            parking se situe sur la gauche et la maison de quartier sur votre
+            droite
+          </Typography>
+        </Stack>
+        <Box>
+          <img
+            src={Image}
+            alt="adress"
+            style={{
+              objectFit: "fill",
+            }}
+          />
+        </Box>
+      </Stack>
+      <Footer />
     </>
-  );;
+  );
 };
 
 export default Contact;
--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -1,11 +1,113 @@
+import { Box, Stack, Typography } from "@mui/material";
 import React from "react";
+import Footer from "../components/footer/Footer";
 import Navbar from "../components/header/Navbar";
+import Image from "../images/adress.png";
 
 const Service = () => {
   return (
     <>
       <Navbar />
-      <div>Services</div>
+      <Typography variant="h1" align="center">
+        Services
+      </Typography>
+      <Stack
+        margin="auto"
+        width={"80%"}
+        display="flex"
+        padding={6}
+        height={"auto"}
+        spacing={10}>
+        <Stack direction="row">
+          <Box>
+            <Typography variant="h2">Escape Game</Typography>
+            <Typography variant="body1">
+              Le jeu d'évasion consiste la plupart du temps à tenter de
+              s’échapper d’une pièce en un temps limité (généralement une heure)
+              et se pratique habituellement en groupe, même si des concepts
+              existent pour une seule personne (par exemple, Lockdown à
+              Singapour)3. Une joueuse d'un jeu d'évasion en train d'examiner un
+              indice. La plupart du temps par groupe de deux à cinq/six
+              participants (bien qu'il existe des enseignes adaptées à des
+              groupes plus grands), les joueurs doivent chercher des indices
+              disséminés dans une ou plusieurs pièces, puis les combiner pour
+              avancer dans la salle. Le but est généralement de sortir d'une
+              pièce (d’où le nom d'escape room), mais il peut aussi être
+              d'apporter une solution à un autre problème, comme résoudre un
+              meurtre ou trouver un trésor4.
+            </Typography>
+          </Box>
+          <Box>
+            <img
+              src={Image}
+              alt="adress"
+              style={{
+                objectFit: "fill",
+              }}
+            />
+          </Box>
+        </Stack>
+        <Stack direction="row">
+          <Box>
+            <img
+              src={Image}
+              alt="adress"
+              style={{
+                objectFit: "fill",
+              }}
+            />
+          </Box>
+          <Box>
+            <Typography variant="h2">Quidditch</Typography>
+            <Typography variant="body1">
+              Le jeu d'évasion consiste la plupart du temps à tenter de
+              s’échapper d’une pièce en un temps limité (généralement une heure)
+              et se pratique habituellement en groupe, même si des concepts
+              existent pour une seule personne (par exemple, Lockdown à
+              Singapour)3. Une joueuse d'un jeu d'évasion en train d'examiner un
+              indice. La plupart du temps par groupe de deux à cinq/six
+              participants (bien qu'il existe des enseignes adaptées à des
+              groupes plus grands), les joueurs doivent chercher des indices
+              disséminés dans une ou plusieurs pièces, puis les combiner pour
+              avancer dans la salle. Le but est généralement de sortir d'une
+              pièce (d’où le nom d'escape room), mais il peut aussi être
+              d'apporter une solution à un autre problème, comme résoudre un
+              meurtre ou trouver un trésor4.
+            </Typography>
+          </Box>
+        </Stack>
+        <Stack direction="row">
+          <Box>
+            <Typography variant="h2">Jeu de Role </Typography>
+            <Typography variant="body1">
+              Le jeu d'évasion consiste la plupart du temps à tenter de
+              s’échapper d’une pièce en un temps limité (généralement une heure)
+              et se pratique habituellement en groupe, même si des concepts
+              existent pour une seule personne (par exemple, Lockdown à
+              Singapour)3. Une joueuse d'un jeu d'évasion en train d'examiner un
+              indice. La plupart du temps par groupe de deux à cinq/six
+              participants (bien qu'il existe des enseignes adaptées à des
+              groupes plus grands), les joueurs doivent chercher des indices
+              disséminés dans une ou plusieurs pièces, puis les combiner pour
+              avancer dans la salle. Le but est généralement de sortir d'une
+              pièce (d’où le nom d'escape room), mais il peut aussi être
+              d'apporter une solution à un autre problème, comme résoudre un
+              meurtre ou trouver un trésor4.
+            </Typography>
+          </Box>
+          <Box>
+            <img
+              src={Image}
+              alt="adress"
+              style={{
+                objectFit: "fill",
+              }}
+            />
+          </Box>
+        </Stack>
+      </Stack>
+
+      <Footer />
     </>
   );
 };
